24e6fb06820d46f106e2b24e026fb5ccf6b68f0a
Karthik Kovi <Karthi.kovi@gmail.com>
2020-03-17 14:23:05 +1030

    Added questions array of objects

---
+++ script.js
@@ -0,0 +1,32 @@
+const start = document.getElementById("start");
+const quiz = document.getElementById("quizConatiner");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const score = 0;
+const highScore = document.getElementById("highScore");
+const timer = document.getElementById("timer");
+
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        choiceA : "Hyper Text Markup Language",
+        choiceB : "Hyperlink Text Markup Language",
+        choiceC : "Home Text Markup Language",
+        correct: "A"
+    },
+    {
+        question : "Which one of these is a JavaScript package manager?",
+        choiceA : "Node.js",
+        choiceB : "Type Script",
+        choiceC : "npm",
+        correct: "C"
+    },
+    {
+        question : "Which tool can you use to ensure code quality?",
+        choiceA : "Angular",
+        choiceB : "RequireJS",
+        choiceC: "ESLint",
+        correct : "C"
+    }
+]
