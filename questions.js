0fe27d805537d4a5475d92a460b9a1001be5450f
Karthik Kovi <Karthi.kovi@gmail.com>
2020-03-26 23:37:30 +1030

    moved questions to a separate js file

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Hyperlink Text Markup Language", "Home Text Markup Language"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: " Which one of these is a JavaScript package manager?",
+        choices: ["npm", "Type Script", "Node.js"],
+        answer: "npm"
+    },
+    {
+        title: 'What is the correct way to call a string’s built-in method?',
+        choices: [
+          "toUpperCase('str')",
+          "'str'.toUpperCase()",
+          "toUpperCase.'str'()",
+          "'str'.toUpperCase"
+        ],
+        answer: "'str'.toUpperCase()"
+    },
+    {
+        title: "What is the outcome of this statement?",
+        choices: [
+          "hi! is printed to the console",
+          "3 is printed to the console",
+          "'hi!'.length will be printed to the console",
+          "1 is printed to the console"
+        ],
+        answer: "3 is printed to the console"
+      }
+]
